852b81130f71b17eec87b81a39017cf28df181dd
shivakrish76 <108416517+shivakrish76@users.noreply.github.com>
2024-06-25 22:21:34 +0530

    Create Tool.java
    Added Tool DTO

---
+++ Demo 06/Tool.java
@@ -0,0 +1,24 @@
+public class Tool {
+    private String code;
+    private String type;
+    private String brand;
+    private double dailyCharge;
+    private boolean chargeOnWeekends;
+    private boolean chargeOnHolidays;
+
+    public Tool(String code, String type, String brand, double dailyCharge, boolean chargeOnWeekends, boolean chargeOnHolidays) {
+        this.code = code;
+        this.type = type;
+        this.brand = brand;
+        this.dailyCharge = dailyCharge;
+        this.chargeOnWeekends = chargeOnWeekends;
+        this.chargeOnHolidays = chargeOnHolidays;
+    }
+
+    public String getCode() { return code; }
+    public String getType() { return type; }
+    public String getBrand() { return brand; }
+    public double getDailyCharge() { return dailyCharge; }
+    public boolean isChargeOnWeekends() { return chargeOnWeekends; }
+    public boolean isChargeOnHolidays() { return chargeOnHolidays; }
+}
